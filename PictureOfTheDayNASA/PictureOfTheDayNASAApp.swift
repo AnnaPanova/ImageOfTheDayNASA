3a245e87673c1b014cb48de73c93dd62e5c97252
Anna Panova <annapanovadev@gmail.com>
2024-06-26 20:59:07 +0200

    Initial Commit

---
+++ PictureOfTheDayNASA/PictureOfTheDayNASAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureOfTheDayNASAApp.swift
+//  PictureOfTheDayNASA
+//
+//  Created by Anna Panova on 26.06.24.
+//
+
+import SwiftUI
+
+@main
+struct PictureOfTheDayNASAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
